388d231c3e76c647a6b453c069235fe29c34a80d
Adam Liechty <adam@doubledutch.me>
2017-09-28 17:10:13 -0700

    fix android binding fetch

--- bindings.js
+++ bindings.js
@@ -1,9 +1,14 @@
 import { NativeModules, Platform } from 'react-native'
 
 export default Platform.select({
-  ios: () => NativeModules.DDBindings,
-  android: () => Object.assign({}, NativeModules.DDBindings, {
-    currentEvent: needsParse ? JSON.parse(NativeModules.DDBindings.currentEvent) : NativeModules.DDBindings.currentEvent,
-    currentUser: needsParse ? JSON.parse(NativeModules.DDBindings.currentUser) : NativeModules.DDBindings.currentUser
-  })
+  ios() {
+    return NativeModules.DDBindings
+  },
+  android() {
+    const needsParse = NativeModules.DDBindings && (typeof NativeModules.DDBindings.currentUser === 'string')
+    return Object.assign({}, NativeModules.DDBindings, {
+      currentEvent: needsParse ? JSON.parse(NativeModules.DDBindings.currentEvent) : NativeModules.DDBindings.currentEvent,
+      currentUser: needsParse ? JSON.parse(NativeModules.DDBindings.currentUser) : NativeModules.DDBindings.currentUser
+    })
+  }
 })()
